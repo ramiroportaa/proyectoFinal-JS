48d0a53051ba410258191bdbe8d95311688809b4
ramiporta <p0rt4csd@gmail.com>
2021-10-13 02:04:47 -0300

    Arreglos en filtros y orden de e-shop

--- js/app-tienda.js
+++ js/app-tienda.js
@@ -6,12 +6,16 @@ escribirProductosHTML(productos, 4);
 escribirModalesHTML(productos);
 
 //Filtros E-Shop.
+//Colocamos el select en el valor "default" al iniciar la pagina asi no queda marcando el ultimo filtro pero sin filtrar realmente los productos (uso JQuery).
+$("#ordenarPor").val('default');
 let productosMostrados = productos;
 //Creamos funcion que cree un array con los productos que pasan el filtro, y luego vacie el html para volver a ejecutar la funcion de escribir los productos por DOM con el nuevo array.
 function filtroGrupo (grupo) {
     productosMostrados = productos.filter(producto => producto.grupo.startsWith(grupo));
     productosHTML.innerHTML = "";
     (!productosMostrados.length) ? productosHTML.innerHTML = `<h4>No se encontraron ${grupo}</h4> ` : escribirProductosHTML(productosMostrados, 4);
+    //Colocamos el select en el valor "default" cada vez que filtramos asi no queda marcando el ultimo filtro pero sin filtrar realmente los productos (uso JQuery).
+    $("#ordenarPor").val('default');
 };
 //Asignamos el evento click a los nodos de cada categoria y llamamos la funcion recien creada con parametro this.id ya que el id de cada nodo es el string que se quiere pasar como condicion del filtro.
 const categorias = document.getElementsByClassName("categorias");
