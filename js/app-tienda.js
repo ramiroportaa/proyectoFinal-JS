db1952fd3f68c84db3dac8a8a6c1eafe382f87d9
ramiporta <p0rt4csd@gmail.com>
2021-10-13 01:53:02 -0300

    filtros E-Shop y orden por select

--- js/app-tienda.js
+++ js/app-tienda.js
@@ -6,11 +6,12 @@ escribirProductosHTML(productos, 4);
 escribirModalesHTML(productos);
 
 //Filtros E-Shop.
+let productosMostrados = productos;
 //Creamos funcion que cree un array con los productos que pasan el filtro, y luego vacie el html para volver a ejecutar la funcion de escribir los productos por DOM con el nuevo array.
 function filtroGrupo (grupo) {
-    const lista = productos.filter(producto => producto.grupo.startsWith(grupo));
+    productosMostrados = productos.filter(producto => producto.grupo.startsWith(grupo));
     productosHTML.innerHTML = "";
-    (!lista.length) ? productosHTML.innerHTML = `<h4>No se encontraron ${grupo}</h4> ` : escribirProductosHTML(lista, 4);
+    (!productosMostrados.length) ? productosHTML.innerHTML = `<h4>No se encontraron ${grupo}</h4> ` : escribirProductosHTML(productosMostrados, 4);
 };
 //Asignamos el evento click a los nodos de cada categoria y llamamos la funcion recien creada con parametro this.id ya que el id de cada nodo es el string que se quiere pasar como condicion del filtro.
 const categorias = document.getElementsByClassName("categorias");
@@ -19,20 +20,19 @@ for (i=0; i< categorias.length; i++) {
         filtroGrupo(this.id);
     });
 };
-
 //Ordenar productos E-Shop (evento del select).
 document.getElementById("ordenarPor").addEventListener("change", function () {
     let productosOrdenado;
     switch (this.value) {
         case "default":
-            productosOrdenado = productos.slice();
+            productosOrdenado = productosMostrados.slice();
             break;
         case "low-high":
             //Uso .slice() para que se almacene un nuevo array en productosOrdenado (y no se haga referencia al array de productos y se modifique este ultimo).
-            productosOrdenado = productos.slice().sort((a,b)=>a.precio-b.precio);
+            productosOrdenado = productosMostrados.slice().sort((a,b)=>a.precio-b.precio);
             break;
         case "high-low":
-            productosOrdenado = productos.slice().sort((a,b)=>b.precio-a.precio);
+            productosOrdenado = productosMostrados.slice().sort((a,b)=>b.precio-a.precio);
             break;
     }
     productosHTML.innerHTML = "";
