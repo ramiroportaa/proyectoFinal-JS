1af1eb516e833b7defe5ec5306f9d894fa512938
ramiporta <p0rt4csd@gmail.com>
2021-10-30 18:42:21 -0300

    Correccion en newsletter

--- js/app-newsletter.js
+++ js/app-newsletter.js
@@ -1,16 +1,19 @@
 const newsletter = document.querySelector("#newsletter");
+const inputNewsletter = document.querySelector("#inputNewsletter");
 newsletter.addEventListener("submit", (e)=>{
     e.preventDefault();
-    Swal.fire({
-        title: `<strong>Suscripcion exitosa</strong>`,
-        icon: 'success',
-        html:
-          `Gracias por suscribirte!` ,
-        showCloseButton: true,
-        showCancelButton: false,
-        showConfirmButton: false,
-        focusConfirm: false,
-        timer: 2000
-      });
+    if (inputNewsletter.value != ""){
+        Swal.fire({
+          title: `<strong>Suscripcion exitosa</strong>`,
+          icon: 'success',
+          html:
+            `Gracias por suscribirte!` ,
+          showCloseButton: true,
+          showCancelButton: false,
+          showConfirmButton: false,
+          focusConfirm: false,
+          timer: 2000
+        });
     newsletter.reset();
+    }
 })
